6c39cd66df2621df02bbdc2da57cf47f3409bde0
ALPIII3 <164693873+ALPIII3@users.noreply.github.com>
2024-03-23 18:42:43 -0500

    Add files via upload

---
+++ FN.java
@@ -0,0 +1,18 @@
+
+public class FN {
+
+	public static void main(String[] args) {
+		// print first name
+		System.out.println("First name: Abigail");
+		// print last name
+		System.out.println("Last name: Sims");
+		// print street address
+		System.out.println("Street address: 420 Garland Dr");
+		// print city
+		System.out.println("City: Lake Jackson");
+		// print zip code
+		System.out.print("Zip code: 77566");
+
+	}
+
+}
